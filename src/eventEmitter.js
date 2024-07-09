10036e3ac8d60d02e614ad6456538d79a01a574a
VladKoroliuk <koroliuk.work@gmail.com>
2024-07-09 23:42:40 +0300

    refactor: add separate event emitter

---
+++ src/eventEmitter.js
@@ -0,0 +1,17 @@
+const subscriptions = {};
+
+export function subscribe(name, callback) {
+  if (!subscriptions[name]) {
+    subscriptions[name] = [callback];
+    return;
+  }
+  subscriptions[name].push(callback);
+}
+
+export function unsubscribe(name, callback) {
+  subscriptions[name] = subscriptions[name].filter((c) => c != callback);
+}
+
+export function emit(name, ...args) {
+  subscriptions[name].forEach((callback) => callback(null, ...args));
+}
